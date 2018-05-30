74ef033bb7447c4f31bab1432fa8b241bd30db1e
llamosa <michael.llamosa@gmail.com>
2018-05-29 20:32:32 -0700

    Update some imports

--- crypto_value.py
+++ crypto_value.py
@@ -5,7 +5,8 @@ import pandas as pd
 import data_driven as dd
 import mach_learn as ml
 from optimizer import ga_optimizer
-
+from scipy.stats import linregress
+import matplotlib.pyplot as plt
 
 import logging
 logging.basicConfig(filename='crypto_predict.log', level=logging.INFO)
@@ -27,6 +28,15 @@ import glob
 import os
 import sys
 import csv
+
+
+from tensorflow.contrib import learn
+from tensorflow.contrib.learn.python import SKCompat
+from sklearn.metrics import mean_squared_error
+
+from tensorflow.contrib import learn
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from lstm_predictor import load_csvdata, lstm_model
 from utilities import all_stats
 
 from random import sample
@@ -61,7 +71,7 @@ data = np.atleast_2d(np.array([dataTable[start:start + look_back] for start in r
 
 sklearn = True
 keras = False
-tf = False
+lstm = False
 
 # turn labels into binary
 y = np.zeros((len(labels), 1)).astype(int)
@@ -79,9 +89,10 @@ np.random.shuffle(ids)
 test_id = ids[0:cut]
 train_id = ids[cut:]
 
+data = (data - np.min(data,axis=0))/(np.max(data,axis=0) - np.min(data,axis=0) )
+
 if sklearn:
     partition=dd.Partition(trainset=train_id,testset=test_id)
-    data = (data - np.min(data,axis=0))/(np.max(data,axis=0) - np.min(data,axis=0) )
     model = dd.Model(data=np.hstack((data,y)), function=ml.RandF(parameters={'n_estimators':250,'min_samples_split':50}), partition=partition, nfo=3)
     #model = dd.Model(data=np.hstack((data, y)),function=ml.Gproc(parameters=({'theta0':1e-2,'thetaL':1e-4,'thetaU':1e-1})), partition=partition,nfo=3)
     #model = dd.Model(data=np.hstack((data,y)),function=ml.Sksvm(parameters={'regularization':100,'sigma':10}),partition=partition)
@@ -104,9 +115,6 @@ if keras:
 
     X_train = data[train_id,:].astype('float32')
     X_test = data[test_id,:].astype('float32')
-
-    X_train = (X_train - np.min(X_train,axis=0) )/np.max(X_train,axis=0)
-    X_test = (X_test - np.min(X_train,axis=0) )/np.max(X_train,axis=0)
 
     print('X_train shape:', X_train.shape)
     print(X_train.shape[0], 'train samples')
@@ -151,3 +159,55 @@ if keras:
 
     print('All stats train:',['{:6.2f}'.format(val) for val in train_stats])
     print('All stats test:',['{:6.2f}'.format(val) for val in test_stats])
+
+
+if lstm:
+    TIMESTEPS = 1
+    RNN_LAYERS = [{'num_units': 400}]
+    DENSE_LAYERS =  [10, 10]
+    TRAINING_STEPS = 5000
+    PRINT_STEPS = TRAINING_STEPS  # / 10
+    BATCH_SIZE = 100
+
+    X={}
+    Y={}
+
+    X, Y = load_csvdata(data_frame['Close'] - data_frame['Open'], TIMESTEPS, seperate=False)
+
+    regressor = SKCompat(learn.Estimator(model_fn=lstm_model(TIMESTEPS, RNN_LAYERS, DENSE_LAYERS), ))
+
+    # create a lstm instance and validation monitor
+    validation_monitor = learn.monitors.ValidationMonitor(X['test'], Y['test'], )
+    # every_n_steps=PRINT_STEPS,)
+    # early_stopping_rounds=1000)
+    # print(X['train'])
+    # print(y['train'])
+
+    SKCompat(regressor.fit(X['train'], Y['train'],
+                           monitors=[validation_monitor],
+                           batch_size=BATCH_SIZE,
+                           steps=TRAINING_STEPS))
+
+    print('X train shape', X['train'].shape)
+    print('y train shape', Y['train'].shape)
+
+    print('X test shape', X['test'].shape)
+    print('y test shape', Y['test'].shape)
+    y_predicted = {}
+    predicted = np.asmatrix(regressor.predict(X['train']), dtype=np.float32)  # ,as_iterable=False))
+    predicted = np.transpose(predicted)
+    y_predicted['train'] = predicted
+
+    predicted = np.asmatrix(regressor.predict(X['test']), dtype=np.float32)  # ,as_iterable=False))
+    predicted = np.transpose(predicted)
+    y_predicted['test'] = predicted
+
+    print(y_predicted['train'].shape)
+
+    plt.scatter(Y['train'],y_predicted['train'])
+    plt.show()
+    plt.scatter(Y['test'], y_predicted['test'])
+    plt.show()
+
+    print('All stats train:',['{:6.2f}'.format(val) for val in linregress(Y['train'],y_predicted['train'])])
+    print('All stats test:',['{:6.2f}'.format(val) for val in linregress(Y['train'], y_predicted['train'])])
